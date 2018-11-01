92045f02f243081e82f6389dcc533da30fcc12aa
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2018-11-01 21:05:52 +1300

    add builder

--- app.rb
+++ app.rb
@@ -10,7 +10,9 @@ require_relative 'config/application'
 
 # use SlackAuthorizer
 enable :sessions
-use OmniAuth::Strategies::Spotify
+use OmniAuth::Builder do
+    provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify user-read-playback-state user-read-currently-playing'
+end
 
 post '/whatsong' do
   'OK'
