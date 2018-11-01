d2e435b80ea5e4165c3d186c118c9d2c1ccf5067
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2018-11-01 20:00:42 +1300

    add spotify api integration

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
+require 'rspotify'
+
 require_relative 'slack_authoriser'
 
 use SlackAuthorizer
 
 post '/whatsong' do
   'OK'
+  artists = RSpotify::Artist.search('Arctic Monkeys')
+  arctic_monkeys = artists.first
+  arctic_monkeys.genres.join(', ')
 end
